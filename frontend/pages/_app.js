a4031cb5c5d1daa7a458ce4135fea7fa60f668b2
Amit Sharma <89769605+dippy79@users.noreply.github.com>
2026-02-28 07:57:57 +0000

    modified:   README.md

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
-import { appWithTranslation } from 'react-i18next'
-import '../lib/i18n'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '../lib/i18n'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Component {...pageProps} />
+    </I18nextProvider>
+  )
 }
 
-export default appWithTranslation(MyApp)
+export default MyApp
